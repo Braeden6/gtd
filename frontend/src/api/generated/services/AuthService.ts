d297f4dfef4e2b0b8e9a1eda37100b66389b0593
Braeden Norman <braeden.norman@trustflight.com>
2025-04-18 12:04:57 -0700

    ci: 🎡 update sdk

--- frontend/src/api/generated/services/AuthService.ts
+++ frontend/src/api/generated/services/AuthService.ts
@@ -2,46 +2,11 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { Body_auth_session_login_auth_jwt_login_post } from '../models/Body_auth_session_login_auth_jwt_login_post';
 import type { OAuth2AuthorizeResponse } from '../models/OAuth2AuthorizeResponse';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import { OpenAPI } from '../core/OpenAPI';
 import { request as __request } from '../core/request';
 export class AuthService {
-    /**
-     * Auth:Session.Login
-     * @param formData
-     * @returns any Successful Response
-     * @throws ApiError
-     */
-    public static authSessionLoginAuthJwtLoginPost(
-        formData: Body_auth_session_login_auth_jwt_login_post,
-    ): CancelablePromise<any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/auth/jwt/login',
-            formData: formData,
-            mediaType: 'application/x-www-form-urlencoded',
-            errors: {
-                400: `Bad Request`,
-                422: `Validation Error`,
-            },
-        });
-    }
-    /**
-     * Auth:Session.Logout
-     * @returns any Successful Response
-     * @throws ApiError
-     */
-    public static authSessionLogoutAuthJwtLogoutPost(): CancelablePromise<any> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/auth/jwt/logout',
-            errors: {
-                401: `Missing token or inactive user.`,
-            },
-        });
-    }
     /**
      * Oauth:Oauth2.Session.Authorize
      * @param scopes
@@ -81,6 +46,57 @@ export class AuthService {
         return __request(OpenAPI, {
             method: 'GET',
             url: '/auth/oauth/authentik/callback',
+            query: {
+                'code': code,
+                'code_verifier': codeVerifier,
+                'state': state,
+                'error': error,
+            },
+            errors: {
+                400: `Bad Request`,
+                422: `Validation Error`,
+            },
+        });
+    }
+    /**
+     * Oauth:Google.Session.Authorize
+     * @param scopes
+     * @returns OAuth2AuthorizeResponse Successful Response
+     * @throws ApiError
+     */
+    public static oauthGoogleSessionAuthorizeAuthOauthGoogleAuthorizeGet(
+        scopes?: Array<string>,
+    ): CancelablePromise<OAuth2AuthorizeResponse> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/auth/oauth/google/authorize',
+            query: {
+                'scopes': scopes,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+    /**
+     * Oauth:Google.Session.Callback
+     * The response varies based on the authentication backend used.
+     * @param code
+     * @param codeVerifier
+     * @param state
+     * @param error
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static oauthGoogleSessionCallbackAuthOauthGoogleCallbackGet(
+        code?: (string | null),
+        codeVerifier?: (string | null),
+        state?: (string | null),
+        error?: (string | null),
+    ): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/auth/oauth/google/callback',
             query: {
                 'code': code,
                 'code_verifier': codeVerifier,
@@ -140,6 +156,52 @@ export class AuthService {
             },
             errors: {
                 400: `Bad Request`,
+                422: `Validation Error`,
+            },
+        });
+    }
+    /**
+     * Logout
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static logoutAuthLogoutPost(): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/auth/logout',
+        });
+    }
+    /**
+     * Get Authorization Url
+     * @returns string Successful Response
+     * @throws ApiError
+     */
+    public static getAuthorizationUrlAuthOauthMobileGoogleAuthorizeGet(): CancelablePromise<Record<string, string>> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/auth/oauth/mobile/google/authorize',
+        });
+    }
+    /**
+     * Auth Callback
+     * Process OAuth callback with PKCE
+     * @param code
+     * @param state
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static authCallbackAuthOauthMobileGoogleCallbackGet(
+        code: string,
+        state: string,
+    ): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/auth/oauth/mobile/google/callback',
+            query: {
+                'code': code,
+                'state': state,
+            },
+            errors: {
                 422: `Validation Error`,
             },
         });
