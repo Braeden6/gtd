508431b891a78d31a640a18f4d055d3a6ec36370
Braeden6 <Braeden6>
2025-06-26 23:24:42 -0700

    feat: 🎸 WIP migration new packages and tan stack router

---
+++ clients/apps/web/src/components/Draggable.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useDraggable } from '@dnd-kit/core';
+import { type KabanItem } from '@/lib/types';
+
+interface DraggableProps {
+  id: string;
+  children: React.ReactNode;
+  item: KabanItem;
+}
+
+export function Draggable({ id, children, item }: DraggableProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    // transform
+  } = useDraggable({ 
+    id,
+    data: {
+      ...item  
+    }
+   });
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ clients/apps/web/src/routes/_main.tsx
@@ -0,0 +1,21 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { Sidebar } from '@/components/layout/Sidebar'
+import { Topbar } from '@/components/layout/Topbar'
+
+export const Route = createFileRoute('/_main')({
+  component: MainLayout,
+})
+
+function MainLayout() {
+  return (
+    <div className="h-screen flex flex-col bg-background">
+      <Topbar />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 p-6 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
+}
